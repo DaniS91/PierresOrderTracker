db3468b24aff197d80c6565837d68deda8b367b9
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-23 15:40:27 -0800

    add static variable to Order class to store a list of Order instances and static method to retrieve the list

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -6,10 +6,15 @@ namespace OrderTracker.Models
   public class Order
   { 
     public string Item { get; set; }
-
+    private static List<Order> _instances = new List<Order> {};
     public Order(string item)
     {
       Item = item;
+      _instances.Add(this);
+    }
+    public static List<Order> GetAll()
+    {
+      return _instances;
     }
   }
 }
