b05ae0dc602df8f1f22c7b90403c432fded44b1a
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-23 15:00:15 -0800

    red phase-2nd test fail: get item string property from Order class

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,18 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void OrderContructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetItem_ReturnsItem_String()
+    {
+      string item = "flour";
+      Order newOrder = new Order(item);
+      string result = newOrder.Item;
+      Assert.AreEqual(item, result);
+    }
+
   }
 }
