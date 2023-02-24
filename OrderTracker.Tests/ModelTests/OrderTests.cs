758b46c1bcbdec3a11142c61f37e204c5e3a9d56
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-23 18:56:55 -0800

    green phase pass #2: (further testing of previously added functionality) successfully retrieves class property

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,19 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void OrderContructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("test", "test2", "test3", 2.000);
+      Order newOrder = new Order("test", "test2", "test3", "2.000");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+    public void GetProperty_ReturnsTitle_String()
+    {
+      string title = "test";
+      string description = "test2";
+      string date = "test3";
+      string price = "2.000";
+      Order newOrder = new Order(title, description, date, price);
+      string result = title;
+      Assert.AreEqual(newOrder.Title, result);
     }
   }
 }
