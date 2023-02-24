cb8ac5bbed660b2f4a4e5674ec2e55e6fd84281b
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-23 19:02:29 -0800

    red phase fail #3: expectedly fails to set new property without setter

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -25,5 +25,19 @@ namespace OrderTracker.Tests
       string result = title;
       Assert.AreEqual(newOrder.Title, result);
     }
+
+    [TestMethod]
+    public void SetProperty_UpdatesTitle_String()
+    {
+      string title = "test";
+      string description = "test2";
+      string date = "test3";
+      string price = "2.000";
+      Order newOrder = new Order(title, description, date, price);
+      string newTitle = "Flour Order";
+      newOrder.Title = newTitle;
+      string result = newOrder.Title;
+      Assert.AreEqual(newTitle, result);
+    }
   }
 }
