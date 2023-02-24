ff1055f54795a8c86261eb81ac1a0869432bf51f
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-23 19:29:55 -0800

    green phase pass #6: successfully creates order with id and returns id

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -9,6 +9,7 @@ namespace OrderTracker.Models
     public string Description { get; set; }
     public string Date { get; set; }
     public string Price { get; set; }
+    public int Id { get; }
     private static List<Order> _instances = new List<Order> {};
     public Order(string title, string description, string date, string price)
     {
@@ -17,6 +18,7 @@ namespace OrderTracker.Models
       Date = date;
       Price = price;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public static List<Order> GetAll()
