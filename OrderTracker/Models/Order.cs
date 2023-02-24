2ee9a1624395359ea59a2dc26e36e8374e61bdfa
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-23 19:11:34 -0800

    green phase pass #4: successfully returns list of orders after implementing teardown method that clears previously-instantiated order objects

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -9,12 +9,23 @@ namespace OrderTracker.Models
     public string Description { get; set; }
     public string Date { get; set; }
     public string Price { get; set; }
+    private static List<Order> _instances = new List<Order> {};
     public Order(string title, string description, string date, string price)
     {
       Title = title;
       Description = description;
       Date = date;
       Price = price;
+      _instances.Add(this);
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
