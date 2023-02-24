9d4c47ad8f2afbea069d06de24728d121bbaaf14
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-23 18:37:03 -0800

    green phase pass #1: creates instance of order after creating constructor with four properties of Order Class

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -5,20 +5,16 @@ namespace OrderTracker.Models
 {
   public class Order
   { 
-    public string Item { get; set; }
-    private static List<Order> _instances = new List<Order> {};
-    public Order(string item)
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Date { get; set; }
+    public double Price { get; set;}
+    public Order(string title, string description, string date, double price)
     {
-      Item = item;
-      _instances.Add(this);
-    }
-    public static List<Order> GetAll()
-    {
-      return _instances;
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
+      Title = title;
+      Description = description;
+      Date = date;
+      Price = price;
     }
   }
 }
