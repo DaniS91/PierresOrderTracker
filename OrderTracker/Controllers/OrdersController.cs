2ff59168c633fc5da4960deeec0566d26f586f2c
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-25 17:48:35 -0800

    add Controller route for creating new Orders

---
+++ OrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using OrderTracker.Models;
+using System.Collections.Generic;
+
+namespace OrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+       Vendor vendor = Vendor.Find(vendorId);
+       return View(vendor);
+    }
+  }
+}
